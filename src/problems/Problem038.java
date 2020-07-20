6d9a77e4f71e1a1159c84bf003493589a176bbd8
Nathan Whitehead <natew@Nathans-MacBook-Air.local>
2020-07-20 14:10:45 -0400

    Problem038 solution

---
+++ src/problems/Problem038.java
@@ -0,0 +1,46 @@
+package problems;
+
+import lib.Library;
+import runner.ProjectEulerRunner;
+
+public class Problem038 implements ProjectEulerRunner {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem038().run());
+    }
+
+    @Override
+    public String run() {
+        String result = largestPandigitalMultiple();
+        return result;
+    }
+
+    /*
+        Thoughts:
+            First, since n >= 2 the fixed integer multiplier must have less than 5 digits.
+            Second, Since the candidate example starts with a 9 (and we know it isn't the largest), the first digit
+                must be a 9.
+            Third, let the fixed integer multiple be x
+                1. If x is 2 digits,
+                    n=3 can at best be 99*1, 99*2, 99*3 = 8 digits
+                    n=4 can at worst be 91*1, 91*2, 91*3, 91*4 = 11 digits
+                2. If x is 3 digits,
+                    n=2 can at best be 999*1, 999*2 = 7 digits
+                    n=3 can at worst be 901*1, 901*2, 901*3 = 11 digits
+                3. If x is 4 digits (only possible solution),
+                    n=2 can at best be 9999*1, 9999*2 = 9 digits
+            Fourth, we can further reduce the search space by considering 9123 and 9876 as the bounds for pandigital
+            multiples
+     */
+    private String largestPandigitalMultiple() {
+        String result = "";
+        for (int i = 9876; i > 9123; i--) {
+            String s = new String("" + i + (2 * i));
+            if (Library.isPandigital(s)) {
+                result = s;
+                break;
+            }
+        }
+        return result;
+    }
+}
